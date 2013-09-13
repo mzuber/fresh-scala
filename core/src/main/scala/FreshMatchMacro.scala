da9f632337b3bb3d3ef5dfaa40e333247fb04785
Martin Zuber <martin.zuber@tu-berlin.de>
2013-09-13 11:32:02 +0200

    Added CaseDef transformer to freshMatch macro.
    This doe not work yet, we still need to adapt the code for the
    transformed patterns of a partial function.

--- core/src/main/scala/FreshMatchMacro.scala
+++ core/src/main/scala/FreshMatchMacro.scala
@@ -29,6 +29,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import scala.language.postfixOps
+import scala.language.reflectiveCalls
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
@@ -49,15 +51,17 @@ object FreshMatchMacro {
     * the pattern matching code
     * {{{
     * freshMatch(expr){
-    *   case Constructor(Abstraction(y, e)) => Abstraction(y, ... e ...)
+    *   case Constructor(Abstraction(y, ...)) => ...
     * }
     * }}}
     * will be transformed into case patterns with explicit swapping:
     * {{{
     * expr match {
-    *   case Constructor(Abstraction(y, e)) => {
+    *   case Constructor(Abstraction(y, e @ ...)) => {
     *     val z = fresh()
-    *     Abstraction(z, swap(z, y, ... e ...))
+    *     Constructor(Abstraction(z, swap(z, y, e))) match {
+    *       case Constructor(Abstraction(y, ...)) => ...
+    *     }
     *   }
     * }
     * }}}
@@ -69,20 +73,57 @@ object FreshMatchMacro {
     val eplicitSwappingTransformer = new Transformer {
 
       override def transformCaseDefs(trees: List[CaseDef]) = trees map {
-	case caseDef @ CaseDef(pattern, guard , body) => {
-	  /* Collect all pattern variables which are bound in an abstraction pattern */
-	  val boundPatternVarTraverser = new Traverser {
+	case CaseDef(pattern, guard , body) => {
 
-	    var boundNames = List()
+	  /*
+	   * Rewrite the pattern, such that
+	   * - the body of an abstraction is aliased with a pattern variable,
+	   * - and all wildcards are aliased with a pattern variable.
+	   * For example, the pattern
+	   * {
+	   *  C(Abstraction(x, ...), _)
+	   * }
+	   * will be transformed into
+	   * {
+	   *  C(Abstraction(x, e1 @ ...), e2)
+	   * }
+	   * Additionally, we collect all pattern variables which are bound in an
+	   * abstraction pattern along the way. The prefix for all new pattern variables,
+	   * i.e., the generated aliases, will be '_e_'.
+	   */
+	  val patternTransformer = new Transformer {
+	    /* The pattern variables which are bound in an abstraction pattern */
+	    var abstractions: Map[Name, Name] = Map()
 
-	    override def traverse(tree: Tree) = tree match {
-	      case q"Fresh.Abstraction[$nameType, ${_}]($name, $body)" => {
-		// Note: quasi quotes do not work in this settint, we need to construct the tree to match against by hand 
+	    /* Pattern variables of the bound names in the order they appear in the pattern */
+	    var boundNames: List[Name] = List()
+
+	    override def transform(tree: Tree) = tree match {
+	      /* Abstraction pattern with aliased body */
+	      case pq"Abstraction($name @ ${_}, $alias @ $body)" => {
+		abstractions = abstractions + (name -> alias)
+		boundNames = boundNames :+ name
+		pq"Abstraction($name, $alias @ ${super.transform(body)})"
 	      }
-	      case _ => super.traverse(tree)
+	      /* Abstraction pattern with non-aliased body */
+	      case pq"Abstraction($name @ ${_}, $body)" => {
+		val alias = newTermName(c.fresh("_e_"))
+		abstractions = abstractions + (name -> alias)
+		boundNames = boundNames :+ name
+		pq"Abstraction($name, $alias @ ${super.transform(body)})"
+	      }
+	      /* Aliased wildcards are not transformed */
+	      case pat @ pq"$name @ ${Ident(nme.WILDCARD)}" => pat
+
+	      /* Wildcards are replaced by a fresh pattern variable */
+	      case pq"_" => Ident(newTermName(c.fresh("_e_")))
+
+	      case _ => super.transform(tree)
 	    }
 	  }
-	  boundPatternVarTraverser.traverse(pattern)
+	  val transformedPattern = patternTransformer.transform(pattern)
+	  val boundNames = patternTransformer.boundNames
+	  val abstractions = patternTransformer.abstractions
 
 	  /*
 	   * Construct value definitions which generate fresh names for each bound
@@ -90,28 +131,98 @@ object FreshMatchMacro {
 	   * produce the following code block, where `z1' and `z2' are fresh variable
 	   * names:
 	   * {
-	   *   val z1: Name[A] = fresh()
-	   *   val z2: Name[A] = fresh()
+	   *   val z1 = x.refresh()
+	   *   val z2 = y.refresh()
 	   * }
+	   * The prefix for all fresh variable names will be '_$_'.
 	   */
+	  val freshNames: Map[Name, TermName] = boundNames map {
+	    case name => (name, newTermName(c.fresh("_$_")))
+	  } toMap
+	  val freshNameDefs: List[ValDef] = freshNames.toList map {
+	    case (boundName, freshName) => q"val $freshName = $boundName.refresh()"
+	  }
 
 	  /*
-	   * Transform the body of the case definition such that:
+	   * Transform the case definition such that:
 	   * - Each of the names bound in an abstraction pattern will be associated
 	   *   with one of the freshly generated names.
-	   * - The expression of each abstraction value will be surrounded by a swap
-	   *   call which swaps the bound name with its corresponding fresh one.
+	   * - The expression bound in the abstraction pattern will be surrounded by
+	   *   a swap call which swaps the bound name with its corresponding fresh one.
+	   * 
 	   * If for example the bound pattern variable `x' is associated with a
-	   * fresh name stored in the variable `z', the expression
+	   * fresh name stored in the variable `z', the case definition
 	   * {
-	   *  Abstraction(x, e)
+	   *  case Abstraction(x, e @ ...) => ...
 	   * }
 	   * will be transformed into
 	   * {
-	   *  Abstraction(z, swap(z, x, e))
+	   *  case Abstraction(x, e @ ...) => {
+	   *    val z = x.refresh()
+	   *    Abstraction(z, swap(z, x, e)) match {
+	   *      case Abstraction(x, e @ ...) => ...
+	   *    }
+	   *  }
+	   * }
+	   * 
+	   * Nested abstraction patterns will be transformed the same way, the case
+	   * definition
+	   * {
+	   *  case Abstraction(x, e1 @ Abstraction(y, e2 @ ...)) => ...
+	   * }
+	   * will be translated into
+	   * {
+	   *  case Abstraction(x, e1 @ Abstraction(y, e2 @ ...)) => {
+	   *    val z1 = x.refresh()
+	   *    val z2 = y.refresh()
+	   *    Abstraction(z1, swap(z1, x, e1)) match {
+	   *      case Abstraction(x, e1 @ Abstraction(y, e2 @ ...)) => {
+	   *        Abstraction(x, Abstraction(z2, swap(z2, y e2)) match {
+	   *          case Abstraction(x, e1 @ Abstraction(y, e2 @ ...)) => ...
+	   *        }
+	   *      }
+	   *    }
+	   *  }
 	   * }
 	   */
-	  caseDef
+	  val transformedBody = {
+
+	    /*
+	     * Construct a value from the pattern and swap the bound name with
+	     * the fresh one in the body of the abstraction.
+	     */
+	    val freshen = (boundName: Name, body: Name, freshName: Name) => {
+	      val constructValueTransformer = new Transformer {
+		override def transform(tree: Tree) = tree match {
+		  /* Freshen the abstraction over the given bound name */
+		  case pq"Abstraction($name @ ${_}, $expr)" => {
+		    if (name == boundName)
+		      q"Abstraction($freshName, swap($freshName, $boundName, $body))"
+		    else
+		      q"Abstraction($name, ${transform(expr)})"
+		  }
+		  /* Aliased wildcards are transformed to regular identifiers */
+		  case pq"$alias @ ${Ident(nme.WILDCARD)}" => q"$alias"
+		  /* In aliased expressions, the alias is removed */
+		  case pq"$alias @ $expr" => q"${transform(expr)}"
+
+		  case _ => super.transform(tree)
+		}
+	      }
+	      constructValueTransformer.transform(transformedPattern)
+	    }
+
+	    /* Construct the (nested) match statement */
+	    boundNames.foldRight(body)(
+	      (name: Name, tree: Tree) => {
+		val freshendValue = freshen(name, abstractions(name), freshNames(name))
+		q"$freshendValue match { case $transformedPattern => $tree }"
+	      }
+	    )
+	  }
+	  
+	  /* Construct a case definition with transformed pattern and body */
+	  CaseDef(transformedPattern, guard, Block(freshNameDefs, transformedBody))
 	}
       }
     }
